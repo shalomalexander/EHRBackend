dfd3f4c738b2fbbee6f39d070c772ac16684db45
shalomalexander <shalomalexander68@gmail.com>
2021-04-21 13:02:12 +0530

    This is shalom

--- backend/appV1/apps.py
+++ backend/appV1/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
 
-class PersonalinfoConfig(AppConfig):
-    name = 'personalInfo'
+# class PersonalinfoConfig(AppConfig):
+#     name = 'personalInfo'
+
+class AppV1Config(AppConfig):
+    name = 'appV1'
