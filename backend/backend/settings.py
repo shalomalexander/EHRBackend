ca6272c330c8972e2b2007ed360bd6e1e043c496
shalomalexander <shalomalexander68@gmail.com>
2021-04-28 00:58:48 +0530

    updated allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'ty19=!&!-d)3lv9ec2%r7m(cd9fx5zgq)&z)ng5!)d8zu1nqe-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.5', '127.0.0.1', 'shalomalexander.pythonanywhere.com/']
+ALLOWED_HOSTS = ['192.168.1.5', '127.0.0.1', 'shalomalexander.pythonanywhere.com']
 
 AUTH_USER_MODEL = "account.User"
 
