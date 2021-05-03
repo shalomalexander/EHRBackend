af0fd359f9a16d836179f0b78eeea78dc48bcfc1
shalomalexander <shalomalexander68@gmail.com>
2021-05-03 20:59:36 +0530

    PATCH and PUT has been added

--- backend/api/views.py
+++ backend/api/views.py
@@ -52,7 +52,15 @@ class PersonalInfoOfSpecificUser(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)                   
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)   
+
+    def put(self, request, pk, format=None):
+        queryset = self.get_object(pk)
+        serializer = serializers.PersonalInfoSerializer(queryset, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)                        
 
 
 class EmergencyInfoList(APIView):
