a016a5109c165861b207ef47e23c8cd31f645642
shalomalexander <shalomalexander68@gmail.com>
2021-05-03 21:43:36 +0530

    Disease Prediction Serializer updated minimum 3 field required

--- backend/prediction/serializers.py
+++ backend/prediction/serializers.py
@@ -5,8 +5,8 @@ class PredictionSerializer(serializers.Serializer):
     s1 = serializers.CharField()
     s2 = serializers.CharField()
     s3 = serializers.CharField()
-    s4 = serializers.CharField()
-    s5 = serializers.CharField()
+    s4 = serializers.CharField(allow_blank=True, required=False)
+    s5 = serializers.CharField(allow_blank=True, required=False)
     
     def create(self, validated_data):
         """
